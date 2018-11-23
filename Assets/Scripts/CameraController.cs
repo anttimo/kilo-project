1aab9a0c68376f992005205bdc982e43b4d51868
Antti Moilanen <anttim@unity3d.com>
2018-11-23 22:52:57 +0200

    Camera positions fixed.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,18 +7,26 @@ public class CameraController : MonoBehaviour
 
     private Camera targetCamera;
 
+    public bool isLeftCamera = true;
+    public float cameraSpeed = 2f;
     void Awake()
     {
         targetCamera = GetComponent<Camera>();
 
         var v3 = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 4, 0, 0));
-        Debug.Log(v3);
-        targetCamera.transform.position = new Vector3(v3.x, transform.position.y, transform.position.z);
+        var x = v3.x;
+        if (!isLeftCamera)
+        {
+            x = -1 * x;
+        }
+
+        targetCamera.transform.position = new Vector3(x, transform.position.y, transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        var offsetX = transform.position.x + Mathf.Sign(transform.position.x) * cameraSpeed / 40;
+        targetCamera.transform.position = new Vector3(offsetX, transform.position.y, transform.position.z);
     }
 }
