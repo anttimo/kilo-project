9a2f8c0ef462a39c424f50d39895f4b3c0f0195e
Antti Moilanen <anttim@unity3d.com>
2018-11-23 22:10:11 +0200

    Two cameras on one scene.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    private Camera targetCamera;
+
+    void Awake()
+    {
+        targetCamera = GetComponent<Camera>();
+
+        var v3 = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 4, 0, 0));
+        Debug.Log(v3);
+        targetCamera.transform.position = new Vector3(v3.x, transform.position.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
