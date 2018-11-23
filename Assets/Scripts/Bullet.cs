93c9dc210e947a1fe650af3d6198d48c64fbed3e
Ruoyu Sun <ruoyu@unity3d.com>
2018-11-24 00:13:30 +0200

    Add enemy

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,20 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour {
+public class Bullet : MonoBehaviour
+{
 
-	public float speed = 20f;
-	public float knockBack = 10.0f;
+    public float speed = 20f;
+    public float knockBack = 10.0f;
     public Rigidbody2D rb;
-	// Use this for initialization 
-	void Start () {
-		rb.velocity = transform.right * speed;
-	}
-
-	void OnTriggerEnter2D (Collider2D col) {
-		if (col.gameObject.name != "Bullet(Clone)") {
-			Destroy(gameObject);
-			//col.gameObject.transform.Translate( new Vector2(knockBack, 0));
-		}
-	}
+    // Use this for initialization 
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
 }
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public GameObject player;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (player.transform.position - transform.position) * 0.1f * Time.deltaTime;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Destroy(col.gameObject);
+        }
+        else if (col.gameObject.tag != "Bullet")
+        {
+            Destroy(col.gameObject);
+            transform.position = new Vector3(
+                -transform.position.x,
+                transform.position.y,
+                transform.position.z);
+        }
+
+    }
+}
