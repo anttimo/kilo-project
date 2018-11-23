7cbbaeec912fa11bfb0a74855e595d0087eb3a8d
Antti Moilanen <anttim@unity3d.com>
2018-11-23 19:14:51 +0200

    Oops forgot

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+    private int level = 1;
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+        InitGame();
+    }
+
+    //Initializes the game for each level.
+    void InitGame()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Loader : MonoBehaviour
+{
+    public GameObject gameManager;
+    public GameObject soundManager;
+
+
+    void Awake()
+    {
+        if (GameManager.instance == null)
+        {
+            //Instantiate gameManager prefab
+            Instantiate(gameManager);
+        }
+
+        if (SoundManager.instance == null)
+        {
+            Instantiate(soundManager);
+        }
+    }
+}
