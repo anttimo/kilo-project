e445b0c9595eea0d563da11a42228b17fd953ff2
Ruoyu Sun <ruoyu@unity3d.com>
2018-11-24 11:16:38 +0200

    Only shoot one side

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -9,14 +9,19 @@ public class Bullet : MonoBehaviour
     public float knockBack = 10.0f;
     public Rigidbody2D rb;
 
-	float lifetime = 0.0f;
-	// Use this for initialization 
-	void Start () {
-		rb.velocity = transform.right * speed;
-	}
+    private Vector3 initialPosition;
 
-	void Update () {
-		lifetime += Time.deltaTime;
-		if (lifetime > 3) Destroy(gameObject);
-	}
+    void Start()
+    {
+        initialPosition = transform.position;
+        rb.velocity = transform.right * speed;
+    }
+
+    void Update()
+    {
+        if (transform.position.x * initialPosition.x < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
