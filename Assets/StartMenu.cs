a449c39c04522e24ea1d4176244edf32f592f546
Antti Moilanen <anttim@unity3d.com>
2018-12-01 15:06:09 +0200

    Refactor the start menu so it's all inside one canvas parent.

---
+++ Assets/StartMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class StartMenu : MonoBehaviour {
+    public static StartMenu instance = null;
+
+    void Awake()
+    {
+        GameManager.instance.SetStartMenu(gameObject);
+    }
+}
